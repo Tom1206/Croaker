af5770c010323db1cc1f99300f5eff14e9cc4c2f
Tom1206 <thomas.millot@etu.unistra.fr>
2015-04-22 14:01:58 +0200

    ajout du système de tweet

--- routes/index.js
+++ routes/index.js
@@ -1,13 +1,20 @@
 var express = require('express');
 var router = express.Router();
 
+var croaks = [];
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('connexion');
 });
 
+router.post('/home', function(req, res, next) {
+    croaks.unshift(req.body.croak);
+    res.redirect('/home')
+});
+
 router.get('/home', function(req, res, next) {
-  res.render('home');
+  res.render('home', { croaks: croaks });
 });
 
 module.exports = router;
